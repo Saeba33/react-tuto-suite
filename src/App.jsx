46f5bb69b67ef9e7a05f3bc5b822d1d4f17ef13e
Charles Proust <konoha33@hotmail.fr>
2025-09-24 08:45:24 +0200

    prepa ex 11

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import { Component } from "react";
-import Profile from "./components/Profile";
-import { ColorContext, UserContext } from "./contexts/MyContext";
+import Count from "./components/Count";
 
-class App extends Component {
-	state = {
-		user: {
-			name: "Lisa",
-			age: 8,
-		},
-	};
-	render() {
-		return (
-			<UserContext.Provider value={this.state.user}>
-				<ColorContext.Provider value={"red"}>
-					<Profile />
-				</ColorContext.Provider>
-			</UserContext.Provider>
-		);
-	}
-}
+const App = () => {
+	return (
+		<div>
+			<Count />
+		</div>
+	);
+};
 
 export default App;
