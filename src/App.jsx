4f7426e40f829b196e4ecb796538e5908d6933ab
Charles Proust <konoha33@hotmail.fr>
2025-09-24 09:56:46 +0200

    useReducer finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import "./App.css";
 import Count from "./components/Count";
 
 const App = () => {
 	return (
-		<div>
+		<div className="flex flex-col items-center justify-center h-screen">
+			<h1 className="mb-12">Hook useReducer</h1>
 			<Count />
 		</div>
 	);
--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -1,5 +1,35 @@
+import { useReducer } from "react";
+
+const initialState = 0;
+
+const reducer = (state, action) => {
+	switch (action) {
+		case "increment":
+			return state + 1;
+		case "decrement":
+			return state - 1;
+		case "reset":
+			return 0;
+		default:
+			return state;
+	}
+};
+
 const Count = () => {
-	return <div>Count</div>;
+	const [count, dispatch] = useReducer(reducer, initialState);
+
+	return (
+		<div className="flex flex-col items-center">
+			<h2 className="text-2xl">
+				Compteur : <span className="font-bold">{count}</span>
+			</h2>
+			<div className="flex gap-2 items-center mt-4">
+				<button onClick={() => dispatch("increment")}>+</button>
+				<button onClick={() => dispatch("decrement")}>-</button>
+				<button onClick={() => dispatch("reset")}>Reset</button>
+			</div>
+		</div>
+	);
 };
 
 export default Count;
